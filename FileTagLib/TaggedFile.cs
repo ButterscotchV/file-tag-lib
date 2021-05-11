0b0472adae33230e26096f34a7ac020479dc7fae
Dankrushen <amberdclose@gmail.com>
2021-05-11 03:20:47 -0400

    Make TaggedFile constructors more intuitive

--- FileTagLib/TaggedFile.cs
+++ FileTagLib/TaggedFile.cs
@@ -5,59 +5,39 @@ namespace FileTagLib
 {
     public class TaggedFile
     {
+        public readonly FileInfo FileInfo;
+
         public readonly string Location;
         public readonly string Name;
         public readonly string Extension;
 
         public readonly string FilePath;
 
-        public readonly long SizeBytes;
-
         public readonly string HashMd5;
+        public readonly long? SizeBytes;
 
         public readonly List<string> Tags;
         public readonly Dictionary<string, object> Metadata;
 
-        public TaggedFile(string location, string name, string extension, long? sizeBytes = null, string? hashMd5 = null, List<string>? tags = null, Dictionary<string, object>? metadata = null)
+        public TaggedFile(FileInfo fileInfo, string hashMd5, long? sizeBytes = null, List<string>? tags = null, Dictionary<string, object>? metadata = null)
         {
-            Location = location;
-            Name = name;
-            Extension = extension;
-
-            FilePath = Path.Join(Location, $"{Name}.{Extension}");
-
-            if (sizeBytes == null)
-            {
-                var fileInfo = new FileInfo(FilePath);
-                SizeBytes = fileInfo.Length;
-            }
-            else
-            {
-                SizeBytes = (long)sizeBytes;
-            }
-
-            HashMd5 = hashMd5 ?? HashUtil.FileMd5Hash(FilePath);
-
-            Tags = tags ?? new List<string>();
-            Metadata = metadata ?? new Dictionary<string, object>();
-        }
-
-        public TaggedFile(string filePath, long? sizeBytes = null, string? hashMd5 = null, List<string>? tags = null, Dictionary<string, object>? metadata = null)
-        {
-            var fileInfo = new FileInfo(filePath);
+            FileInfo = fileInfo;
 
             Location = fileInfo.DirectoryName!;
             Name = fileInfo.Name;
             Extension = fileInfo.Extension;
 
-            FilePath = filePath;
+            FilePath = fileInfo.FullName;
 
-            SizeBytes = sizeBytes ?? fileInfo.Length;
-
-            HashMd5 = hashMd5 ?? HashUtil.FileMd5Hash(fileInfo);
+            HashMd5 = hashMd5;
+            SizeBytes = sizeBytes;
 
             Tags = tags ?? new List<string>();
             Metadata = metadata ?? new Dictionary<string, object>();
+        }
+
+        public TaggedFile(FileInfo fileInfo, List<string>? tags = null, Dictionary<string, object>? metadata = null) : this(fileInfo, HashUtil.FileMd5Hash(fileInfo), fileInfo.Length, tags, metadata)
+        {
         }
     }
 }
