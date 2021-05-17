ae30db6f79462c68e3b708ba8732441c03160305
Dankrushen <amberdclose@gmail.com>
2021-05-17 03:43:19 -0400

    Add TaggedFile tests

---
+++ FileTagLibTests/TaggedFileTests.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Xunit;
+
+namespace FileTagLib.Tests
+{
+    public class TaggedFileTests
+    {
+        [Theory]
+        [InlineData("HashTest.txt", "40F6C9227AAB58F1D36BDF2954571A50", 39)]
+        public void TaggedFileTest(string path, string md5Hash, long fileSize)
+        {
+            var taggedFile = new TaggedFile(new FileInfo(path));
+
+            Assert.Equal(md5Hash, taggedFile.HashMd5);
+            Assert.Equal(fileSize, taggedFile.SizeBytes);
+        }
+    }
+}
